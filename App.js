ac3013d790d3a91dba53990a57636dbc476aaed8
RianVitor26 <rianvitorlhe@gamil.com>
2023-01-07 21:17:35 -0300

    feat: desenvolvendo estrutura básica do form

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { styles } from './styles';
+import { Text, TextInput, View, Button } from "react-native";
+import { styles } from "./styles";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text
+          accessibilityLabel="Click me for write your height"
+          accessibilityHint="Focus on the input for height"
+          accessibilityState={{
+            selected: false,
+          }}
+        >Height</Text>
+        <TextInput keyBoardType="numeric"  placeholder="Ex: 1.80"></TextInput>
+        <Text
+          accessibilityLabel="Click me for write your weight"
+          accessibilityHint="Focus on the input for weight"
+          accessibilityState={{
+            selected: false,
+          }}
+        >Weight</Text>
+        <TextInput keyBoardType="numeric"  placeholder="Ex: 80.5"></TextInput>
+          <Button title="Calculator my BMI" />
+      </View>
     </View>
   );
 }
-
-
